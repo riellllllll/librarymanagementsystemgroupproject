5d9ba5140ee4dfa4a47fdad8317802035cb5ef42
rexriel <eddgabrielebadilla@gmail.com>
2026-05-23 02:26:21 +0800

    remove borrowed_books.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -82,15 +82,6 @@ function _nav_active(string $href, string $current): string {
     </a>
 
     <div class="nav-section-label">My Library</div>
-    <a href="borrowed_books.php" class="nav-link<?= _nav_active('borrowed_books.php', $_s_page) ?>">
-      <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-        <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/>
-      </svg>
-      Borrowed Books
-      <?php if ($_s_borrows > 0): ?>
-        <span class="nav-badge"><?= $_s_borrows ?></span>
-      <?php endif; ?>
-    </a>
     <a href="borrow_history.php" class="nav-link<?= _nav_active('borrow_history.php', $_s_page) ?>">
       <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
         <polyline points="12 8 12 12 14 14"/><path d="M3.05 11a9 9 0 1 0 .5-4"/>
