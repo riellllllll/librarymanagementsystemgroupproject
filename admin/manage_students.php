d5c6d03cc17c6bc629282c2dc68a4be546b7f87f
rexriel <eddgabrielebadilla@gmail.com>
2026-05-28 01:43:48 +0800

    add adminStyle.css to manage_students.php and upda te books.xml with new book entry

--- admin/manage_students.php
+++ admin/manage_students.php
@@ -44,6 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="../assets/student.css">
 <link rel="stylesheet" href="../assets/managestudent.css">
+<link rel="stylesheet" href="../assets/adminStyle.css">
 </head>
 <body>
 
